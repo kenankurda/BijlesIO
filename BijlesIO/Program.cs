ae83d705bfd674865d007e518b8541c9860781b0
kenan <kenan@LAPTOP-5KTTCAE1>
2021-02-18 15:51:44 +0100

    Add project files.

---
+++ BijlesIO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BijlesIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"d:\test\Dummy.txt";
+            //Console.Write("Input your text : ");
+            //string line = Console.ReadLine();
+            //WriteToFile(line, path);
+
+            string line =  ReadFromFile(path);
+
+
+            Console.WriteLine(line);
+        }
+       
+        public static void WriteToFile(string line, string path)
+        {
+            FileStream fileStream = new FileStream(path, FileMode.Append, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+
+            streamWriter.WriteLine(line);
+            streamWriter.Flush();
+            fileStream.Flush();
+            streamWriter.Close();
+            fileStream.Close();
+        }
+
+        public static string ReadFromFile(string path)
+        {
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStream);
+
+            string line = streamReader.ReadToEnd();
+            //Console.WriteLine(line);
+            fileStream.Flush();
+            fileStream.Close();
+            
+            streamReader.Close();
+            return line;
+            
+
+        }
+    }
+}
---
+++ FileIOForTest/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOForTest
+{
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "log1.txt";
+
+            string line = "This is additional text" + DateTime.Now;
+            List<Person> list = new List<Person>() 
+            { 
+              new Person { FirstName="Kenan", LastName="Kurda", Age=50},
+              new Person { FirstName="Saif", LastName="Al Adilee", Age=25},
+              new Person { FirstName="Jarno", LastName="Maes", Age=22},
+              new Person { FirstName="Francis", LastName="Bulu", Age=18}
+            };
+
+            //WriteListToFile(list, path);
+
+            WriteToFile(line,path);
+
+        }
+
+        public static void WriteListToFile(List<Person> people, string path) 
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fileStream))
+            {
+                foreach (var item in people)
+                {
+                    sw.WriteLine($"First Name: {item.FirstName}, Last Name: {item.LastName}, Age: {item.Age}");
+                }
+            }
+        }
+
+        public static void WriteToFile(string line, string path)
+        {
+           
+            using (FileStream fileStream = new FileStream(path, FileMode.Append, FileAccess.Write))
+            using (StreamWriter streamWriter = new StreamWriter(fileStream))
+            {
+                streamWriter.WriteLine(line);
+            }
+         
+        }
+    }
+}
